82cbe46829962abc479dcc42af97c43101f5bb75
Robert205 <robertmihai300@gmail.com>
2022-09-13 12:56:30 +0300

    Add background color to divs and made them show up one after another

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 content = document.querySelector("container")
 //function to create 16x16 grid of divs column
 function createDiv(){
-for (var i = 0; i <= 31; i++) {
-container.innerHTML += '<div class="content"></div>';
+  for (var i = 0; i <= 31; i++) {
+container.innerHTML += '<div style="background-color:cyan;" class="content">Test</div>';
+
     }
 }
 createDiv();
